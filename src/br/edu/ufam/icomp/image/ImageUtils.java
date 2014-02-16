6240e751c729f07a231c4a2a664ee97d70361417
Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>
2014-02-16 15:40:52 -0400

    Adding experiment 02 - SplitTransformer
    Signed-off-by: Luiz Carlos Cavalcanti <cavalcanti.luiz@gmail.com>

--- src/br/edu/ufam/icomp/image/ImageUtils.java
+++ src/br/edu/ufam/icomp/image/ImageUtils.java
@@ -1,6 +1,7 @@
 package br.edu.ufam.icomp.image;
 
 import java.awt.Color;
+import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 public class ImageUtils {
@@ -22,7 +23,7 @@ public class ImageUtils {
         return new Color((int) (redBucket / pixelCount), (int) (greenBucket / pixelCount),
                 (int) (blueBucket / pixelCount));
     }
-    
+
     public static int[] generateBWHistogram(BufferedImage bitmap, int bucketCount) {
         int[] histogram = new int[bucketCount];
         for (int y = 0; y < bitmap.getHeight(); y++) {
@@ -35,5 +36,26 @@ public class ImageUtils {
         }
         return histogram;
     }
-    
+
+    // TODO configure split number
+    public static BufferedImage[] splitImage(BufferedImage img) {
+        int chunkHeight = img.getHeight() / 2;
+        int chunkWidth = img.getWidth() / 2;
+        int verticalPieces = img.getHeight() / chunkHeight;
+        int horizontalPieces = img.getWidth() / chunkWidth;
+        BufferedImage result[] = new BufferedImage[4];
+        int piece = 0;
+        for (int x = 0; x < verticalPieces; x++) {
+            for (int y = 0; y < horizontalPieces; y++) {
+                BufferedImage chunk = new BufferedImage(chunkWidth, chunkHeight, img.getType());
+                Graphics2D g = chunk.createGraphics();
+                g.drawImage(img, 0, 0, chunkWidth, chunkHeight, chunkWidth * y, chunkHeight * x, chunkWidth * y
+                        + chunkWidth, chunkHeight * x + chunkHeight, null);
+                result[piece] = chunk;
+                piece++;
+            }
+        }
+        return result;
+    }
+
 }
